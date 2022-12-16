0011f0df720fca6b63eae8afa16927add81ba4a6
sanadab <sanadab@ac.sce.ac.il>
2022-12-16 05:05:47 +0200

    Sanad Commit

---
+++ Register.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const employeeSchema = new mongoose.Schema({
+    firstname : {
+        type:String,
+        required:true
+    },
+    lastname : {
+        type:String,
+        required:true
+    },
+    email : {
+        type:String,
+        required:true
+    },
+    gender : {
+        type:String,
+        required:true
+    },
+    age : {
+        type:String,
+        required:true
+    },
+    password : {
+        type:String,
+        required:true
+    },
+    confirmpassword : {
+        type:String,
+        required:true
+    }
+})
+
+const Register =  new mongoose.model("Register",employeeSchema);
+
+module.exports = Register;
--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require("express")
 var bodyParser = require("body-parser")
 var mongoose = require("mongoose")
+const User = require("./User")
 
 const app = express()
+
 
 app.use(bodyParser.json())
 app.use(express.static('public'))
@@ -18,16 +20,25 @@ mongoose.connect('mongodb+srv://sanadab7:PasswordPassword@cluster0.qxmzvmg.mongo
 
 
 var db = mongoose.connection;
+
+db.on('error', () => console.log("Error in connecting to database"));
+db.once('open', () => console.log("Connected to Databases"))
+
 app.get("/", (req, res) => {
     res.render("login")
 
 })
 
-
-
-
-db.on('error', () => console.log("Error in connecting to database"));
-db.once('open', () => console.log("Connected to Databases"))
+app.post("/Log-in", (req, res) => {
+    var check = db.collection('User').findOne();
+    console.log(check.password);
+    console.log(req.body.password);
+    if (check.password === req.body.password) {
+        res.redirect("/Home.html")
+    } else {
+        res.send("Wrong Password")
+    }
+});
 
 app.post("/Sign-Up", (req, res) => {
     var _id = req.body._id;
@@ -47,21 +58,33 @@ app.post("/Sign-Up", (req, res) => {
         "password": password
     }
 
-    db.collection('Users').insertOne(data, (err, collection) => {
+
+
+
+
+    db.collection('users').insertOne(data, (err, collection) => {
         if (err) {
             throw err;
         }
         console.log("Record Inserted Successfuly");
     });
 
-    return res.redirect('Log-in.html')
+    return res.redirect('Home.html')
 
 })
 
 
 
+//const collection = new mongoose.model("collection",userSchema)
 
+//module.exports=collection
+app.get("/", (req, res) => {
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
 
-.listen(5000);
+    return res.redirect('/Home.html');
+
+}).listen(5000);
 
 console.log("Listening on PORT 5000");

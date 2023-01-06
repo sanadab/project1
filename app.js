77bbf379a238ecfe35ebcd6a75f85529fa4213a7
MohamadWattad <abotalebwattad@gmail.com>
2023-01-06 19:35:37 +0200

    delete commented code

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const ejs = require("ejs");
 var engines = require('consolidate');
 const { send } = require("process");
 
-
-
 app.set('view engine', 'ejs');
 app.use(express.static('views'));
 app.set('views', __dirname + '/views');
@@ -18,12 +16,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-
-
 const User = require('./Database/DBs/User.js').User
-// const Request= require('./Database/DBs/Request.js').Request
-
-
 
 app.get("/", (req, res) => {
     res.render("Home.html")
@@ -48,9 +41,6 @@ app.get('/profile-cos', function(req, res) {
 app.get('/Profile-Service1', function(req, res) {
     res.render('Profile-Service1.ejs');
 });
-// app.get('/Customer-Donation-Request', function(req, res) {
-//     res.render('Customer-Donation-Request.html');
-// });
 
 app.get('/Employees', function(req, res) {
     User.find({}, function(err, users) {
@@ -70,24 +60,10 @@ app.get('/Customer-details', function(req, res) {
     });
 });
 
-// app.get('/Profile-Service1', function(req, res) {
-//     User.find({}, function(err, users) {
-        
-//         res.render('Profile-Service1.ejs', {
-
-//             s: users
-//         });
-
-//     });
-// });
-
 app.post('/Log-In', (req, res) => {
-
     try {
-
         User.findOne({
             id: req.body.id,
-
         }, function(err, user) {
             if (err) { // user doesn't exist
                 res.json({
@@ -95,9 +71,6 @@ app.post('/Log-In', (req, res) => {
                 })
             }
             if (user) { //user exist
-
-
-
                 if (req.body.password === user.password) {
                     console.log(user);
                     console.log("\n inside the login\n");
@@ -108,15 +81,11 @@ app.post('/Log-In', (req, res) => {
                     if (user.Roll === 'Admin') {
                         return res.redirect("/profile");
                     }
-
-
                     if (user.Roll === 'customer') {
                         return res.redirect("/profile-cos");
                     }
                     // req.session.user = user;
                     console.log("asas");
-
-
                 } else {
                     return res.redirect("/Log-in");
                 }
@@ -183,53 +152,11 @@ app.post("/Sign-Up", (req, res) => {
             return res.redirect("/Sign-Up");
         }
     });
-
-
 });
-// app.post('/Customer-Donation-Request', (req, res) => {
-//     const Name = req.body.FirstName;
-//     const Email = req.body.email;
-//     const Request = req.body.Request;
-//   console.log("asd ");
-//     const client = new MongoClient(uri, { useNewUrlParser: true });
-//     client.connect(err => {
-//       const collection = client.db("DB").collection("Request");
-//     //   console.log("asd");
-//       collection.insertOne({ Name: FirstName, Email: email, Request: Request }, function(err, res) {
-//         console.log("Request saved to database");
-//         client.close();
-//       });
-//     });
-//   });
-// app.post('/Customer-Donation-Request', (req, res) => {
-//     // Create a new instance of the Request model
-//     const Request = new Request({
-//       Firstname: req.body.Firstname,
-//       Email: req.body.Email,
-//       Request: req.body.Request
-//     });
-//     // Save the customer request to the database
-//     request.save((error) => {
-//       if (error) {
-//         // If there is an error, send a response with a status code of 500
-//         res.sendStatus(500);
-//       } else {
-//         // If the request was saved successfully, send a response with a status code of 200
-//         res.sendStatus(200);
-//       }
-//     });
-//   });
 
-
-// app.delete('/Log-out', function(req, res) {
-//     req.session.destroy(function(err) {
-//         res.redirect('/Log-in.html');
-//     });
-// });
 app.get('/Log-out', (req, res) => {
     req.session.destroy();
     res.redirect('/Log-in');
 });
-
 
 module.exports = app;

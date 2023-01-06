bc072a6fd14625a90789dd4cee4b7ac64d45861f
MohamadWattad <abotalebwattad@gmail.com>
2023-01-06 21:50:23 +0200

    improvments

--- Database/database.js
+++ Database/database.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const DB_URI = process.env.MONGODB_URL || "mongodb+srv://sanadab7:PasswordPassword@cluster0.qxmzvmg.mongodb.net/?retryWrites=true&w=majority";
 
-
-
 function connect() {
     return new Promise((resolve, reject) => {
 

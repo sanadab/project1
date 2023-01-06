ad1cc7fe6dcfe4ccfd02df5b0f77a042a81b3fe5
MohamadWattad <abotalebwattad@gmail.com>
2023-01-06 19:24:14 +0200

    reorginize repo

--- Backend/dal/dal.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-//connection to database
-mongoose.connect(config.mongodb.connectionString)
-    .then(db => console.log("DB connected"))
-    .catch(err => console.log(err));

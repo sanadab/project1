0c4532fad1ec69bf21d2001e5cb01d5e28a66f4d
sanadab <sanadab@ac.sce.ac.il>
2023-01-14 20:12:09 +0200

    add dbtestunits.js with  5 functions for test (Profile-cos.htlm,Home.html,profile.ejs,Log-in.html,sign-up.html)

--- dbtestunits.js
+++ dbtestunits.js
@@ -2,23 +2,20 @@ var request = require('supertest');
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let should = chai.should();
-const app = require('../../../app.js');
-const conn = require('../../../db/database.js');
+const app = require('./app.js')
+const conn = require('./Database/database.js');
 
-let server = require('../../../server.js');
+let server = require('./server.js');
 var expect = chai.expect;
 chai.use(chaiHttp);
 
 var test = require('mocha').test;
-
-
-
 //1
 
 describe('Check if the routes goes to login page!', function() {
 
     test('responds to /Log-In', async() => {
-        const res = await request(server).get('/Log-In');
+        const res = await request(app).get('/Log-in.html');
         expect(res.should.have.status(200));
     });
 });
@@ -27,74 +24,37 @@ describe('Check if the routes goes to login page!', function() {
 describe('Check if the routes goes to register page!', function() {
 
     test('responds to /Sign-Up', async() => {
-        const res = await request(server).get('/Sign-Up');
+        const res = await request(app).get('/Sign-Up.html');
         expect(res.should.have.status(200));
     });
 });
 
 //3
 
-describe('Check if the routes goes to Home page!', function() {
+describe('Check if the routes goes to profile page!', function() {
 
-    test('responds to /Home', async() => {
-        const res = await request(server).get('/Home');
+    test('responds to /profile', async() => {
+        const res = await request(app).get('/profile.ejs');
         expect(res.should.have.status(200));
     });
 });
 
 //4
-describe('Check if the routes goes to Sign Up As Patient!', function() {
 
-    test('responds to /Sign-Up_As_Patient', async() => {
-        const res = await request(server).get('/Sign-Up_As_Patient');
+describe('Check if the routes goes to Home Page', function() {
+
+    test('responds to /Home', async() => {
+        const res = await request(app).get('/Home.html');
         expect(res.should.have.status(200));
     });
-});
 
+});
 //5
-describe('Check if the routes goes to Sign Up As Examinator!', function() {
+describe('Check if the routes goes to customer profile Page', function() {
 
-    test('responds to /Sign-Up_As_Examinator', async() => {
-        const res = await request(server).get('/Sign-Up_As_Examinator');
-
+    test('responds to /Profile-cos', async() => {
+        const res = await request(app).get('/Profile-cos.html');
         expect(res.should.have.status(200));
     });
-});
 
-//6
-describe('Check if the routes goes to Sign Up As Doctor!', function() {
-
-    test('responds to /Sign-Up_As_Doctor', async() => {
-        const res = await request(server).get('/Sign-Up_As_Doctor');
-        expect(res.should.have.status(200));
-    });
-});
-
-//7
-describe('Check if the routes goes to ForgotPW', function() {
-
-    test('responds to /ForgotPW', async() => {
-        const res = await request(server).get('/ForgotPW');
-        expect(res.should.have.status(200));
-    });
-});
-
-
-//8
-describe('Check if the routes goes to BloodTestValues', function() {
-
-    test('responds to /BloodTestValues', async() => {
-        const res = await request(server).get('/BloodTestValues');
-        expect(res.should.have.status(200));
-    });
-});
-
-//9
-
-describe('Check if the routes goes to EditDoctor', function() {
-
-    test('responds to /EditDoctor', async() => {
-        const res = await request(server).get('/EditDoctor');
-        expect(res.should.have.status(200));
-    });
 });
